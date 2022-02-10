e62f376f5904bed8d016160e4c8966833c2e8850
Alakeks <mateuszkostka5@gmail.com>
2022-02-10 23:37:45 +0100

    Comments

--- book_library_app/__init__.py
+++ book_library_app/__init__.py
@@ -5,9 +5,16 @@ from flask_migrate import Migrate
 app = Flask(__name__)
 app.config.from_object(Config)
 
+#creating instance of SQLALCHEMY object and binding it with flask object
 db = SQLAlchemy(app)
+
+#configure alembic to work with flask instance and db
 migrate=Migrate(app,db)
 
+#authors is responsible to connect logigc and www 
 from book_library_app import authors
+#models is responsible for creating and manage data model in db
 from book_library_app import models 
+#manage is custom logic using click framework to execute custom CLI comands 
+#responsible for adding or remove sample data from database
 from book_library_app import db_manage_commends
--- book_library_app/db_manage_commends.py
+++ book_library_app/db_manage_commends.py
@@ -5,20 +5,27 @@ import json
 from pathlib import Path
 from datetime import datetime
 
+
+#flask cli group 
 @app.cli.group()
 def db_manage():
     """Database managment commends"""
     pass
 
+
+#function to add data to database from sample 
 @db_manage.command()
 def add_data():
     """Add sample data to database"""
+    #connecting with db in case something went wrong with connection
     try:
         authors_path=Path(__file__).parent/'samples'/'authors.json'
         with open(authors_path) as file:
             data_json=json.load(file)
         for item in data_json:
+            # in sample data there date is in dd-mm-yyyy 
             item['birth_date']=datetime.strptime(item['birth_date'],'%d-%m-%Y').date()
+            #to create instance of a Author from key world argument
             author=Authors(**item)
             db.session.add(author)
         db.session.commit()
@@ -31,6 +38,7 @@ def add_data():
 def remove_data():
     """Remove all data from database"""
     try:
+        #SQL comant to deleta date from db 
         db.session.execute('TRUNCATE TABLE authors')
         print('Data has been sucessfully removed from database')
         db.session.commit()
