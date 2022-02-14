1c90dff1de4b291bf15080354b1ab61a22bd521f
Alakeks <mateuszkostka5@gmail.com>
2022-02-14 01:34:16 +0100

    Add database model for book ; update for db_manage_commands

--- book_library_app/commands/db_manage_commends.py
+++ book_library_app/commands/db_manage_commends.py
@@ -1,9 +1,18 @@
 from book_library_app import db
-from book_library_app.models import Author
+from book_library_app.models import Author, Book
 import json
 from pathlib import Path
 from datetime import datetime
 from book_library_app.commands import db_manage_bp
+
+
+def load_json_data(file_name:str)->list:
+        authors_path=Path(__file__).parent.parent/'samples'/file_name
+        with open(authors_path) as file:
+            data_json=json.load(file)
+        return data_json
+
+
 
 #flask cli group 
 @db_manage_bp.cli.group()
@@ -18,15 +27,21 @@ def add_data():
     """Add sample data to database"""
     #connecting with db in case something went wrong with connection
     try:
-        authors_path=Path(__file__).parent.parent/'samples'/'authors.json'
-        with open(authors_path) as file:
-            data_json=json.load(file)
+        data_json=load_json_data('authors.json')
         for item in data_json:
             # in sample data there date is in dd-mm-yyyy 
             item['birth_date']=datetime.strptime(item['birth_date'],'%d-%m-%Y').date()
             #to create instance of a Author from key world argument
             author=Author(**item)
             db.session.add(author)
+
+        data_json=load_json_data('books.json')
+        for item in data_json:
+            # in sample data there date is in dd-mm-yyyy 
+            #to create instance of a Author from key world argument
+            book=Book(**item)
+            db.session.add(book)
+
         db.session.commit()
         print('Data has been sucessfully added to database')
     except Exception as exc:
@@ -38,8 +53,8 @@ def remove_data():
     """Remove all data from database"""
     try:
         #SQL comant to deleta date from db 
-        db.session.execute('TRUNCATE TABLE authors RESTART IDENTITY')
-        print('Data has been sucessfully removed from database')
+        db.session.execute('TRUNCATE TABLE authors RESTART IDENTITY CASCADE;')
         db.session.commit()
+        print('Data has been sucessfully removed from database')
     except Exception as exc:
         print(f'Unexcepted error: {exc}')
--- book_library_app/models.py
+++ book_library_app/models.py
@@ -25,6 +25,7 @@ class Author(db.Model):
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     birth_date = db.Column(db.Date, nullable=False)
+    books=db.relationship('Book',back_populates='author',cascade="all, delete-orphan")
 
     def __repr__(self) -> str:
         return f'<{self.__class__.__name__}>: {self.first_name}{self.last_name}'
@@ -105,6 +106,18 @@ class Author(db.Model):
 
         return paginate_object.items, pagination
 
+class Book(db.Model):
+    __tablename__='books'
+    id= db.Column(db.Integer,primary_key=True)
+    title=db.Column(db.String(50),nullable=False)
+    isbn=db.Column(db.BigInteger,nullable=False,unique=True)
+    number_of_pages=db.Column(db.Integer,nullable=False)
+    description=db.Column(db.Text,nullable=False)
+    author_id=db.Column(db.Integer,db.ForeignKey('authors.id'),nullable=False)
+    author=db.relationship('Author',back_populates='books')
+
+    def __repr__(self):
+        return f'{self.title}-{self.author.first_name} {self.author.last_name} '
 
 class AuthorSchema(Schema):
     id = fields.Integer(dump_only=True)
